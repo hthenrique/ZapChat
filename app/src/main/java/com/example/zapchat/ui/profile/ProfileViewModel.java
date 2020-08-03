457ca43f3914975d771b03bdd76565646b061867
hthenrique <ht.henrique@live.com>
2020-08-03 19:26:48 -0300

    0.1

---
+++ app/src/main/java/com/example/zapchat/ui/profile/ProfileViewModel.java
@@ -0,0 +1,19 @@
+package com.example.zapchat.ui.profile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ProfileViewModel(){
+        mText = new MutableLiveData<>();
+        mText.setValue("Profile Name");
+    }
+
+    public LiveData<String> getText(){
+        return mText;
+    }
+}
